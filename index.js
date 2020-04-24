2a3d6f024289d7cabf113ede90b11523f67be341
Chris Carr <christocarr@outlook.com>
2020-04-24 14:21:13 +0100

    Add render to render newsTitles to index.ejs

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express()
 const port = process.env.PORT || '8000'
 const apiKey = 'c3d70ad0232d4ecfb9bed0c791c22710'
 
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -15,10 +17,12 @@ app.get("/", async (req, res) => {
   const data =  await fetch(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${apiKey}`)
   const jsonData = await data.json()
   const articles = jsonData.articles
-  const newTitles = articles.map(article => {
+  const newsTitles = articles.map(article => {
     return article.title
   })
-  res.json(newTitles)
+  res.render('index', {
+    newsTitles: newsTitles
+  })
 })
 
 app.listen(port, () => {
