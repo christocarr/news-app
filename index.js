b677b9e6954ca71599f6ecbebc738b243695f7d7
Chris Carr <christocarr@outlook.com>
2020-04-24 12:58:16 +0100

    Add api key

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const app = express()
 const port = process.env.PORT || '8000'
+//https://newsapi.org/v2/top-headlines?country=gb&apiKey=c3d70ad0232d4ecfb9bed0c791c22710
+const apiKey = 'c3d70ad0232d4ecfb9bed0c791c22710'
 
 app.get("/", (req, res) => {
   res.status(200).send('Hi')
