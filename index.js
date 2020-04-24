51199dc37b598332907f9e8dcb011e2e47035ddc
Chris Carr <christocarr@outlook.com>
2020-04-24 13:14:52 +0100

    Add fetch to news api

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const path = require('path')
+const fetch = require('node-fetch')
 
 const app = express()
 const port = process.env.PORT || '8000'
 //https://newsapi.org/v2/top-headlines?country=gb&apiKey=c3d70ad0232d4ecfb9bed0c791c22710
 const apiKey = 'c3d70ad0232d4ecfb9bed0c791c22710'
 
-app.get("/", (req, res) => {
-  res.status(200).send('Hi')
+app.use(express.json())
+
+app.get("/", async (req, res) => {
+  const data =  await fetch(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${apiKey}`)
+  const json = await data.json()
+  res.json(json.status)
 })
 
 app.listen(port, () => {
