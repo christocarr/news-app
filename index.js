1d82a317df4f94c80436a4a61e92064afdd7a4db
Chris Carr <christocarr@outlook.com>
2020-04-25 10:46:44 +0100

    Add css file

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const apiKey = 'c3d70ad0232d4ecfb9bed0c791c22710'
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
